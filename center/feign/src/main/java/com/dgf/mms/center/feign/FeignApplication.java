26cab4771773645b8807f858f71b0244f8b1e266
dwx5984 <dwx5984@users.noreply.github.com>
2018-11-08 00:09:03 +0800

    设置eureka-server双节点，以实现高可用；添加feign模块（未完成）。

---
+++ center/feign/src/main/java/com/dgf/mms/center/feign/FeignApplication.java
@@ -0,0 +1,19 @@
+package com.dgf.mms.center.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @date 2018-11-07 22:00:38
+ * @author dgf
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+}
--- modules/customer/src/main/java/com/dgf/mms/modules/customer/CustomerApplication.java
+++ modules/customer/src/main/java/com/dgf/mms/modules/customer/CustomerApplication.java
@@ -3,6 +3,7 @@ package com.dgf.mms.modules.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableFeignClients
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
