728cf59e589afe024a619093c507762b12c82cb9
dwx5984 <dwx5984@users.noreply.github.com>
2018-11-08 23:42:41 +0800

    完成feign模块和customer模块的整合模板，其他模块尚未完成

--- modules/customer/src/main/java/com/dgf/mms/modules/customer/CustomerApplication.java
+++ modules/customer/src/main/java/com/dgf/mms/modules/customer/CustomerApplication.java
@@ -3,9 +3,12 @@ package com.dgf.mms.modules.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @date 2018-11-04 00:09:10
@@ -14,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@EnableFeignClients
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
@@ -24,4 +26,5 @@ public class CustomerApplication {
     public String hello(){
         return "hello customer1";
     }
+
 }
